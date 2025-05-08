d3c8341d95eba7bb4feba738fa306f9838e1b013
Z-Isaac08 <ezechielncho225@gmail.com>
2025-05-08 10:06:18 +0000

    Project Euler: Probleme 1

---
+++ problem-1/problem-1-multiples-of-3-or-5.js
@@ -0,0 +1,14 @@
+function multiplesOf3Or5(number) {
+  let somme = 0;
+
+  for (let i = 0; i < number; i++) {
+    if (i % 3 === 0 || i % 5 ===0) {
+      somme += i ;
+    }
+  }
+
+  return somme;
+}
+
+multiplesOf3Or5(1000);
+
